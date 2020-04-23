0f3525505b8cfb69822ada3f6f1646f1885bc157
onaimat <omarnaimat@hotmail.com>
2020-04-23 14:39:59 +0300

    add conflict

--- src/main.java
+++ src/main.java
@@ -2,6 +2,6 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello Atypon");
-        System.out.println("Employee 2 Commit");
+        System.out.println("Employee 2 Commit"); //conflict
     }
 }
