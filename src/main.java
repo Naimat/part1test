28a606e2174ecda3329a355f633c1b49aabc4e19
onaimat <omarnaimat@hotmail.com>
2020-04-23 04:41:38 -0700

    subbranch1

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello Atypon");
-        System.out.println("Employee 2 Commit"); //conflict
+        System.out.println("Employee 2 Commit");
+        System.out.println("SubBranch1 commit");
     }
 }
